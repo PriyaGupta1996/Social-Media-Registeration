91777c460873b3c04a26365c48670c8a4416bd56
PriyaGupta1996 <priyagupta.ynr@gmail.com>
2023-02-12 00:24:16 -0500

    1) Sign in route. 2) Return message and token only on successful login and user creation

--- routes/auth.js
+++ routes/auth.js
@@ -29,9 +29,12 @@ router.post('/signup', async (req, res) => {
             let userDetailsResponse = await userDetails.save()
             if (userDetailsResponse) {
                 const token = jwt.sign(req.body.email.toLowerCase(), process.env.JWT_TOKEN)
-                const { password, ...result } = userDetailsResponse._doc
-                result.token = token
-                res.status(200).send({ message: result })
+                //        const { password, ...result } = userDetailsResponse._doc
+                //        result.token = token
+                res.status(200).send({
+                    message: "Successfully created user",
+                    token: token
+                })
             }
 
         }
@@ -49,28 +52,29 @@ router.post('/signup', async (req, res) => {
 
 })
 
-// router.post("/signin", (req, res) => {
-//     user.find({ email: req.body.email }, async (err, result) => {
-//         if (err)
-//             res.status(500).send({ message: err })
-//         if (result) {
-//             async
-//             let salt = await bcrypt.genSalt(process.env.SALT_ROUNDS)
-//             password = await bcrypt.hash(req.body.password, salt)
-//             bcrypt.compare(password, result.password, (err, response) => {
-//                 if (err)
-//                     res.status(500).send({ message: err })
-//                 else if (response == true) {
-//                     res.redirect("/")
-//                 }
-//                 else {
-//                     res.status(404).send({ message: "Incorrect password" })
-//                     res.redirect("/")
-//                 }
-//             })
-//         }
-//     })
-// })
+router.post("/signin", async (req, res) => {
+    try {
+        let result = await user.find({ email: req.body.email.toLowerCase() })
+        if (result.length > 0) {
+            let valid = await bcrypt.compare(req.body.password, result[0].password)
+            if (valid === true) {
+                const token = jwt.sign(req.body.email.toLowerCase(), process.env.JWT_TOKEN)
+                res.status(200).json({
+                    message: "Successful login",
+                    token: token
+                })
+            }
+            else {
+                return res.status(404).send({ message: "Incorrect password" })
+            }
+        } else {
+            return res.status(404).send({ message: "User not found" })
+        }
+    }
+    catch (err) {
+        return res.status(500).send({ error: err.message })
+    }
+})
 
 
 module.exports = router
