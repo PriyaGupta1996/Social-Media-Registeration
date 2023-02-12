6196d53a7afcbd1b4e490993469577f3755153c5
PriyaGupta1996 <priyagupta.ynr@gmail.com>
2023-02-12 11:26:27 -0500

    1) Fix : multiple try catch 2) Fix : required fields error

--- routes/auth.js
+++ routes/auth.js
@@ -6,55 +6,42 @@ const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 router.post('/signup', async (req, res) => {
-    let result = await user.find({ email: req.body.email.toLowerCase() })
+    if (!req.body.email || !req.body.password)
+        return res.status(400).send({ message: "Email and password are required." })
+    const result = await user.find({ email: req.body.email.toLowerCase() })
     if (result.length > 0) {
         return res.status(400).send({ message: "email already exists" })
     }
-    let password
     try {
-        let salt = await bcrypt.genSalt(+process.env.SALT_ROUNDS)
-        password = await bcrypt.hash(req.body.password, salt)
-    }
-    catch (err) {
-        return res.status(500).json({ error: err.message })
-    }
-    try {
+        const salt = await bcrypt.genSalt(+process.env.SALT_ROUNDS)
+        const password = await bcrypt.hash(req.body.password, salt)
         const userDetails = new user({
             name: req.body.name,
             email: req.body.email.toLowerCase(),
             password: password
         })
-
-        try {
-            let userDetailsResponse = await userDetails.save()
-            if (userDetailsResponse) {
-                const token = jwt.sign(req.body.email.toLowerCase(), process.env.JWT_TOKEN)
-                //        const { password, ...result } = userDetailsResponse._doc
-                //        result.token = token
-                res.status(200).send({
-                    message: "Successfully created user",
-                    token: token
-                })
-            }
-
+        const userDetailsResponse = await userDetails.save()
+        if (userDetailsResponse) {
+            const token = jwt.sign(req.body.email.toLowerCase(), process.env.JWT_TOKEN)
+            //        const { password, ...result } = userDetailsResponse._doc
+            //        result.token = token
+            res.status(200).send({
+                message: "Successfully created user",
+                token: token
+            })
         }
-        catch (err) {
-            console.log(err)
-            return res.status(500).json({ error: err.message })
-        }
-
     }
     catch (err) {
         console.log(err)
         res.status(500).json({ error: err.message })
     }
-
-
 })
 
 router.post("/signin", async (req, res) => {
     try {
-        let result = await user.find({ email: req.body.email.toLowerCase() })
+        if (!req.body.email || !req.body.password)
+            return res.status(400).send({ message: "Email and password are required." })
+        let result = await user.find({ email: req.body.email?.toLowerCase() })
         if (result.length > 0) {
             let valid = await bcrypt.compare(req.body.password, result[0].password)
             if (valid === true) {
@@ -65,10 +52,10 @@ router.post("/signin", async (req, res) => {
                 })
             }
             else {
-                return res.status(404).send({ message: "Incorrect password" })
+                return res.status(400).send({ message: "Incorrect password" })
             }
         } else {
-            return res.status(404).send({ message: "User not found" })
+            return res.status(400).send({ message: "User not found" })
         }
     }
     catch (err) {
